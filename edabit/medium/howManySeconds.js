30dabb976453dea93ed5a1d6269666c854a14073
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-10 09:13:10 +0000

    search.js finished

---
+++ edabit/medium/howManySeconds.js
@@ -0,0 +1,11 @@
+// Write a function that converts hours into seconds.
+
+// pseudocode
+// declare a function howManySeconds and give one parameter 'hours'
+// inside the function, multiply parameter hours by 3600 and assign the returned value to a const variable seconds;
+// return the value of seconds 
+
+function howManySeconds(hours) {
+    const seconds = hours * 3600;
+    return seconds;
+}
