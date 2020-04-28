7047a128798ccef6dc359aa02696359c27a37bb6
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-28 06:11:29 +0000

    made note on nSideShape.js

--- edabit/hard/nSideShape.js
+++ edabit/hard/nSideShape.js
@@ -41,3 +41,22 @@ function nSidedShape (n) {
             return 'please input numbers between 1 and 10';
     }
 }
+
+
+// shorter version:
+// note: we can instantiate a variable with predefined sets of properties to hold the desired value, then return the variable at nth property to get the desired value;
+function nSidedShape(n) {
+	const shapes = {
+		1	:"circle",
+		2	:"semi-circle",
+		3	:"triangle",
+		4	:"square",
+		5	:"pentagon",
+		6	:"hexagon",
+		7	:"heptagon",
+		8	:"octagon",
+		9	:"nonagon",
+		10:"decagon",
+	}
+	return shapes[n]
+}
