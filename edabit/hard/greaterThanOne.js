092ec5da02b2f9d28eb810ff7941c0bae4cf017e
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-04-29 06:45:10 +0000

    completed greaterThanOne.js

--- edabit/hard/greaterThanOne.js
+++ edabit/hard/greaterThanOne.js
@@ -2,14 +2,16 @@
 
 // Pseudocode:
 // declare a function called greaterThanOne with a single parameter, string
-// instantiate a variable called fraction;
-// use the Number method passing in string as an argument and assign the returned value to a variable named fraction;
-// set a condition checking if fraction is greater than 1;
+// instantiate a variable called numerator and demoninator;
+// assign the value of 0th index of the returned value of the split method of the string passing in value '/' as an argument to the numerator
+// assign the value of 1st index of the returned value of the split method of the string passing in value '/' as an argument to the denominator
+// set a condition checking if numerator divided by denominator is strictly greater than 1;
 // if true, return true;
 // otherwise, return false;
 
 function greaterThanOne (string) {
-    const fraction = Number(string);
-    if(fraction > 1) return true;
+    const numerator = string.split('/')[0]
+    const denominator = string.split('/')[1]
+    if(numerator/denominator > 1) return true;
     return false;
 }
