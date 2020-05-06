622b4296ab50536a8cc674f99a7fe4539c5cd6c2
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-06 06:43:56 +0000

    completed firstUniqueCharacter.js

---
+++ leetcode/monthlyChallenge/may/firstUniqueCharacter.js
@@ -0,0 +1,19 @@
+// Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+// Pseudocode:
+// declare a function called firstUniqueCharacter with a single parameter, string;
+// iterate the process as many as string's length many times,
+// within the iteration, check if the character at current iteration index is NOT included in the string parameter AFTER the strings of the current index (if on index 3, then 0-3 wont count)
+// if true, return the current index i of the iteration
+// if false, return -1;
+
+var firstUniqChar = function(string) {
+    let result = 0;
+    for(let i = 0; i < string.length; i++) {
+        if(!string.includes(string[i], i+1)) {
+            result = string.indexOf(string[i]);
+            return result;
+        }
+    }
+    return result;
+};
