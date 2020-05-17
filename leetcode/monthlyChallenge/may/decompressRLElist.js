112133bdda12393c11fe598181a725366ba08588
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-17 03:33:09 +0000

    completed decompressRLElist.js

---
+++ leetcode/monthlyChallenge/may/decompressRLElist.js
@@ -0,0 +1,20 @@
+// We are given a list nums of integers representing a list compressed with run-length encoding.
+// Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+// Pseudocode:
+// Declare a function called decompressRLElist with a single parameter nums;
+// within the function, set a loop that iterates length of the num object many times;
+// set the incrementor to increment by 2 on each iteration
+// 
+
+var decompressRLElist = function(nums) {
+    const resultArr = [];
+    for(let i = 0; i < nums.length; i += 2) {
+        let freq = nums[i]
+        let value = nums[i+1]
+        for(let j = 0; j < freq; j++) {
+            resultArr.push(value);
+        }
+    }
+    return resultArr;
+};
