21e881a3b20568c5cbf7aac6f621185094fcd962
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-21 07:14:16 +0000

    revised mergedTwoLists.js

--- leetcode/monthlyChallenge/may/mergeTwoLists.js
+++ leetcode/monthlyChallenge/may/mergeTwoLists.js
@@ -2,10 +2,25 @@
 
 // Pseudocode:
 // declare a function called mergeTwoLists with 2 parameters l1 and l2;
-// instantiate a variable called resultArr with an initial value of an empty string;
-// instantiate a variable called newL1 and assign it a returned value of next property of l1
-// instantiate a variable called newL1 and assign it a returned value of next property of l2
-// set a loop that iterates while 
+// instantiate a variable called resultNode and assign it a new Class of ListNode
+// instantiate a variable called currentNode and give it an iniitial value of resultNode;
+// set a loop that iterates while l1 and l2 is valid
+    // set a condition to check if val property at l1 is less than val property at l2
+        // if true, assign currentNode.next a value of l1
+        // assign l1 a next property of itself (this is to move to the next list)
+        // otherwise, assign currentNode.next a value of l2
+        // assign l2 a next property of itself (this is to move to the next list)
+    // set currentNode to returned value of next property of the currentNode object (this is to move to the next like of the list)
+
+// set a loop that iterates while l1 OR l2 is valid (this is to take care of the remaining items on either l1 or l2)
+    // set a condition to check if l1 is valid
+        // if true, assign currentNode.next a value of l1
+        // assign l1 a next property of itself (this is to move to the next list)
+        // otherwise, assign currentNode.next a value of l2
+        // assign l2 a next property of itself (this is to move to the next list)
+    // set currentNode to returned value of next property of the currentNode object (this is to move to the next like of the list)
+
+// Finally, return the next property of resultNode object (reason for next to is to remove the unwanted 0, as the list starts with the val 0);
 
 var mergeTwoLists = function(l1, l2) {
     const resultNode = new ListNode();
@@ -31,5 +46,5 @@ var mergeTwoLists = function(l1, l2) {
         }
         currentNode = currentNode.next;
     }
-    return resultNode
+    return resultNode.next
 };
