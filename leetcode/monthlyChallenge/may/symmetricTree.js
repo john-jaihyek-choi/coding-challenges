97a7caf3a32d69f08d7acd33bdcdf2de8f72b919
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-26 06:54:42 +0000

    completed symmetricTree.js

--- leetcode/monthlyChallenge/may/symmetricTree.js
+++ leetcode/monthlyChallenge/may/symmetricTree.js
@@ -4,15 +4,26 @@
 // each time I traverse through the node, 
     // check if:
         // left of the left node is equal to right of the right node
-        // right of the left node is equal to left of the right node
-// for the above to be possible, I need to keep track of the following:
-    // currentNode or depth of the tree that I am currently on
-    // 
+        // right of the left node is equal to left of the right node 
 
 // Pseudocode:
 // declare a function called isSymmetric with a single parameter root
-// instantiate a variable called left with an initial value of root.left
-// instantiate a variable called right with an initial value of root.right
+// instantiate a variable called checkStatus with an initial value of true;
+    // set a guard to check if root is a falsy value
+        // if true, return the variable checkStatus
+    // declare a function called checkSymmetry with 2 parameters, left and right
+        // within the function set a condition to check if either left or right is a falsy value
+            // if true,
+                // set a condition to check if left is equal to right
+                    //if true, return out of the function
+                    // otherwise, set checkStatus value to false and return out of the function
+        // set another condition to check if val of left and val of right is not equal to each other
+            // if true, set checkStatus equal to false and return out of the function
+        // call checkSymmetry function passing left.left and right.right as an argument
+        // call checkSymmetry function passing left.right and right.left as an argument
+    // call checkSymmetry function passing in root.left and root.right
+    // return checkStatus;
+
 
 // first solution:
 var isSymmetric = function(root) {
