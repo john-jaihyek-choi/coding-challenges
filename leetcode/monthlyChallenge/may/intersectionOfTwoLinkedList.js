d722f518d6eb5166b2e8315af037704d3874dd92
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-28 08:46:58 +0000

    completed second solution for intersectionOfTwoLinkedList.js

--- leetcode/monthlyChallenge/may/intersectionOfTwoLinkedList.js
+++ leetcode/monthlyChallenge/may/intersectionOfTwoLinkedList.js
@@ -34,7 +34,7 @@
     // return null if nothing is returned during the iteration above;
     
 
-// first solution (works, but inefficient):
+// first solution (works, but inefficient) [ about 200 ms average ]:
 var getIntersectionNode = function(headA, headB) {
     let headAList = headA;
     let headBList = headB;
@@ -47,6 +47,79 @@ var getIntersectionNode = function(headA, headB) {
         if(checkListArr.includes(headBList)) {
             return headBList.val;
         }
+        headBList = headBList.next;
+    }
+    return null;
+};
+
+// second solution (more efficient way) [ about 100 ms average ]:
+// I will first find the length of of both set of lists
+// then whichever is a longer list out of the two, I will advance by the difference between the longest list's length and shorter lists's length
+    // this will make the list now same length which can solve my initial concern about two lists having different lengths;
+
+// Pseudocode:
+// declare a function called getIntersectionNode with 2 parameters headA and headB
+// instantiate a variable called countA and countB with an initial value of 0
+// instantiate a variable called headAList with an initial value of headA
+// instantiate a variable called headBList with an initial value of headB
+// set a loop that iterates while headAList is valid
+    // increment countA on every iteration then set headAList to next property of the headAList
+// set a loop that iterates while headBList is valid
+    // increment countB on every iteration then set headBList to next property of the headBList
+// after the count has been recorded, instantiate a variable called diff and set it equal to the returned value of the following:
+    // returned value of calling max method of the Math object passing in 2 arguments:
+        // countA - countB
+        // countB - countA
+    // ** this will return which ever is positive as a value;
+// now that I've got the differences, I now know how much to advance for the longer list;
+// set headAList and headBList both equal to its according heads in parameter again;
+// set a condition to check if countA is greater than countB
+    // if true, 
+        // set a loop that iterates diff many times;
+            // in each iteration, set headAList equal to next property of the headAList;
+    // otherwise,
+        // set a loop that iterates diff many times;
+            // in each iteration, set headAList equal to next property of the headBList;
+// set a last loop that iterates while headA or headB (either one) is invalid;
+    // set a condition to check if headAList reference at current iteration is equal to headBList reference of the current iteration
+        // if true, return that headAList
+    // on every iteration set headAList AND headBList to its own next property;
+// if nothing is returned in the above loop, return null;
+
+var getIntersectionNode = function(headA, headB) {
+    let countA = 0;
+    let countB = 0;
+    let headAList = headA;
+    let headBList = headB;
+
+    while(headAList) {
+        countA++;
+        headAList = headAList.next;
+    }
+    while(headBList) {
+        countB++;
+        headBList = headBList.next;
+    }
+    
+    const diff = Math.max(countA - countB, countB - countA);
+    headAList = headA;
+    headBList = headB;
+    
+    if(countA > countB) {
+        for(let i = 0; i < diff; i++) {
+            headAList = headAList.next;
+        }
+    } else{
+        for(let i = 0; i < diff; i++) {
+            headBList = headBList.next;
+        }
+    }
+
+    while(headAList) {
+        if(headAList == headBList) {
+            return headAList;
+        }
+        headAList = headAList.next;
         headBList = headBList.next;
     }
     return null;
