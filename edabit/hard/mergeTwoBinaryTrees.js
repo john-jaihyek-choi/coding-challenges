0ef066da8d4536aabc091262e9d9cf66f651c95e
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-05-30 21:45:38 +0000

    completed mergeTwoBinaryTrees.js with pseudocode

--- edabit/hard/mergeTwoBinaryTrees.js
+++ edabit/hard/mergeTwoBinaryTrees.js
@@ -10,25 +10,28 @@
 // some of conditions to keep in mind:
     // tree 1 is null and tree 2 is not: Set tree1 to tree2 val
     // tree 2 is null and tree 1 is not: Keep the tree1 val
-    // tree 1 is null and tree 2 is null: exit out of function
+    // otherwise, tree1 val is equal to sum of tree1 and tree2 val
 
 
 // Pseudocode:
 // declare a function called mergeTrees with 2 set of parameters, t1 and t2
-    // instantiate a variable called t1Head with an initial value of t1
-    // instantiate a variable called t2Head with an initial value of t2
-    // set a loop that iterates while t1Head is valid;
-        // set a condition to check if t2Head is valid
-        // if true, set t1Head to sum of t1Head and t2Head;
-        // otherwise, 
+    // set a conditon to check if t1 is invalid
+        // if true, return t2
+    // set a condition to check if t2 is invalid
+        // if true, return t1
+    // set t1.val equal to t1.val plus t2.val
+    // set t1.left equal to calling recursive mergeTrees function passing t1.left and t2.left as an arguemnt
+    // set t1.right equal to calling recursive mergeTrees function pssing t1.right and t2.right as an argument
+
+    // return t1;
 
 var mergeTrees = function(t1, t2) {
     if(!t1) return t2;
     if(!t2) return t1;
 
     t1.val = t1.val + t2.val;
-    mergeTrees(t1.left, t2.left)
-    mergeTrees(t1.right, t2.right);
+    t1.left = mergeTrees(t1.left, t2.left)
+    t1.right = mergeTrees(t1.right, t2.right);
 
     return t1;
 };
