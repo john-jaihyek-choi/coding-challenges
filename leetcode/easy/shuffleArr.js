058b4580bb77edeb547e4e8c7e7f6b60c51a88b1
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-06-08 22:44:45 +0000

    revised comments on the code

--- leetcode/easy/shuffleArr.js
+++ leetcode/easy/shuffleArr.js
@@ -44,7 +44,7 @@ var shuffle = function(array, n) { // time / space complexity
     return resultArr;
 }; 
 
-// solution with improved readability and slightly improved runtime;
+// solution with improved readability
 var shuffle = function(array, n) { // time / space complexity
     const resultArr = []; // O(1) / O(1)
     for(let i = 0; i < n; i++) {
