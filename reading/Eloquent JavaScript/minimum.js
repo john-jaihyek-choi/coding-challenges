eeffe030d4d765879a6f6831c58d957ae44d2a87
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-07-08 07:45:00 +0000

    completed solution for min.js

---
+++ reading/Eloquent JavaScript/minimum.js
@@ -0,0 +1,14 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(num1, num2) {
+    const first = Number(num1);
+    const second = Number(num2);
+
+    if(first > second) {
+      return second;
+    } else {
+      return first;
+    }
+}
+
+console.log(min(0, 10));
