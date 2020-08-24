b73e9701592dcd2e61b601896664f84832dcd7c5
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-08-24 04:23:25 +0000

    completed overlappingLetters.js

---
+++ robin/overlappingLetters.js
@@ -0,0 +1,18 @@
+// Write a function that receives two strings and returns a string that contains only the letters that are shared between both strings. If there are none, return an empty string. Put the string in alphabetical order before returning it.
+
+function overlappingLetters (string1, string2) {
+    const string1Letters = {};
+    const result = ''
+    
+    for(let i = 0; i < string1.length; i++) {
+      string1Letters[string1[i]] = true;
+    }
+
+    for(let i = 0; i < string2.length; i++) {
+        if(string1Letters[string2[i]]) result += string2[i];
+    }
+
+    return result;
+}
+
+console.log(overlappingLetters('abc', 'abd'));
