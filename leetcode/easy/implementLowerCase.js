d683fb1cf959ca789cb91528a1de12851973155c
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-10-07 06:03:57 +0000

    added description

--- leetcode/easy/implementLowerCase.js
+++ leetcode/easy/implementLowerCase.js
@@ -1,4 +1,5 @@
-// 
+// #709
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
 
 var toLowerCase = function(str) {
     let newStr = ''
